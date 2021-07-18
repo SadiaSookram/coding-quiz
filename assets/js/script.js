047eb4d6f8edc31010de65f9d1c97cb7ac151990
SadiaSookram <sadiamsookram@gmail.com>
2021-07-18 13:47:52 -0400

    Displaying questions in content box added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@
 
 var quiz = document.getElementById("quiz-container");
 var getResultsbutton = document.getElementById("get-results");
-var onQuestion = 0;
 var timer  = document.getElementById("timer")
 var timeLeft = 500;
 var score = 0;
 var results= [];
-
+var onQuestion = 0;
+var answers = 0;
 // Quiz Questions //
 
 var quizQuestions = [
@@ -15,6 +15,7 @@ var quizQuestions = [
         question: "What is considered the (Skeleton) of every webpage ?",
         answers: ["CSS", "JAVASCRIPT", "HTML", "GIT"],
         correct: 3
+        
     },
     {
         question: "What is the <Title> tag used for ?",
@@ -38,6 +39,12 @@ var quizQuestions = [
     }
 ];
 
+console.log(quizQuestions[0]);
+console.log(quizQuestions[1]);
+console.log(quizQuestions[2]);
+console.log(quizQuestions[3]);
+console.log(quizQuestions[4]);
+
 // Adding timer through quiz
 
 function quizTimer() {
@@ -52,13 +59,20 @@ function quizTimer() {
 update = setInterval("quizTimer()" , 1000)
 
 // if the user selets next run the quiz 
-var quiz = document.getElementById("heading");
+var quiz = document.getElementById("quizcontent");
+var next = document.getElementById("nextButton")
+// Creating quiz function
+function quizExecute () {
 
-// For loop through questions 
- for(var i=0; i < quizQuestions.length; i++ ) {
- 
+    // Display question from array
+    document.getElementById("quizcontent").innerHTML
+     = quizQuestions[onQuestion].question
+    // create buttons for answer choices
+  
+} 
 
-}
+    
+
      
 
     
